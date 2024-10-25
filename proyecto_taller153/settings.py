ce6a13a090a7b9ac4dd200ac4363ca8fa1e0af65
Richard <ricky201325@gmail.com>
2024-10-25 04:35:42 -0300

    antes de instalar los rquerimientos

--- proyecto_taller153/settings.py
+++ proyecto_taller153/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +81,7 @@ WSGI_APPLICATION = 'proyecto_taller153.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
