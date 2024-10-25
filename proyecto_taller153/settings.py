b5317eb1548323aa0197b3851697e48d1111a702
Richard <ricky201325@gmail.com>
2024-10-25 02:10:28 -0300

    s

--- proyecto_taller153/settings.py
+++ proyecto_taller153/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gc$9lh%cjzd2ubw6^!s1!+5k%m-t!=qg&s3xofrnm2-f*k4c8c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['127.0.0.1','localhost','https://proyectotaller153-production.up.railway.app']
+ALLOWED_HOSTS = ['proyectotaller153-production.up.railway.app', '127.0.0.1', 'localhost']
 
 # Application definition
 
