34e6edf6a9705ecfc94f85b55d65d73becc2a35c
Richard <ricky201325@gmail.com>
2024-10-27 20:46:14 -0300

    se añadio la validacion de email en la bd

--- login/views.py
+++ login/views.py
@@ -6,6 +6,8 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login  # Renombramos login para evitar conflicto
 from django.conf import settings
+from django.contrib.auth import get_user_model
+
 
 def login(request):
     return render(request, 'login.html')
@@ -27,35 +29,53 @@ def login_view(request):
     return render(request, "login.html")
 
 def recuperar_contraseña(request):
+    message = None
+    message_type = None
+    show_captcha = True  # Variable para controlar la visibilidad del captcha
+    
     if request.method == 'POST':
         form = FormularioRecuperar(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             
-            # Construir la URL absoluta
-            recuperacion_url = request.build_absolute_uri(
-                reverse('contraseña_recuperacion')
-            )
-            
-            # Modificar el mensaje del correo para incluir el enlace
-            mensaje_correo = f'''
-            Para recuperar tu contraseña, haz clic en el siguiente enlace:
-            {recuperacion_url}
-            '''
-            send_mail(
-                'Recuperación de Contraseña',
-                mensaje_correo,
-                'ricky201325@gmail.com',
-                [email],
-                fail_silently=False,
-            )
-            message = f'Correo enviado a {email} con instrucciones para recuperar la contraseña.'
-            return render(request, 'correo_recuperacion.html', {'form': form, 'message': message})
+            # Verificar si existe el usuario con ese email
+            User = get_user_model()
+            try:
+                user = User.objects.get(email=email)
+                
+                # Si existe el usuario, envía el correo
+                recuperacion_url = request.build_absolute_uri(
+                    reverse('contraseña_recuperacion')
+                )
+                mensaje_correo = f'''
+                Para recuperar tu contraseña, haz clic en el siguiente enlace:
+                {recuperacion_url}
+                '''
+                send_mail(
+                    'Recuperación de Contraseña',
+                    mensaje_correo,
+                    'ricky201325@gmail.com',
+                    [email],
+                    fail_silently=False,
+                )
+                message = f'Correo enviado a {email} con instrucciones para recuperar la contraseña.'
+                message_type = 'success'
+                show_captcha = False  # Ocultar captcha después de envío exitoso
+            except User.DoesNotExist:
+                message = 'No existe ninguna cuenta asociada a este correo electrónico.'
+                message_type = 'error'
+                show_captcha = False  # Ocultar captcha después de error
+                
     else:
         form = FormularioRecuperar()
 
-    return render(request, 'correo_recuperacion.html', {'form': form,'RECAPTCHA_SITE_KEY': settings.RECAPTCHA_SITE_KEY})
-
+    return render(request, 'correo_recuperacion.html', {
+        'form': form,
+        'RECAPTCHA_SITE_KEY': settings.RECAPTCHA_SITE_KEY,
+        'message': message,
+        'message_type': message_type,
+        'show_captcha': show_captcha
+    })
 def mostrar_template_recuperacion(request):
     return render(request, 'contraseña_recuperacion.html')
 
