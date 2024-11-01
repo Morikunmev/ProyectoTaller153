4a6125466514a5df2c750a3a6b84875874ff819a
Richard <ricky201325@gmail.com>
2024-10-31 23:13:46 -0300

    se saco el logo

--- proyecto_taller153/settings.py
+++ proyecto_taller153/settings.py
@@ -130,9 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'  # Español de Chile
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'  # Zona horaria de Chile
 
 USE_I18N = True
 
